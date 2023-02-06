2844912352eda4377829ed20ca57eb30225f2042
verenichyi <verenichyi@gmail.com>
2023-02-06 17:22:32 +0300

    prepare environment

---
+++ js/practice/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "node": true,
+        "mocha": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": 2015
+    },
+    "rules": {
+    }
+}
