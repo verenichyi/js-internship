769138339ac0c093af4a92ce6e5f486b03fe236d
verenichyi <verenichyi@gmail.com>
2023-02-07 18:29:03 +0300

    feat: implement `isCreditCardNumber` func

--- js/practice/task/06-conditions-n-loops-tasks.js
+++ js/practice/task/06-conditions-n-loops-tasks.js
@@ -293,7 +293,42 @@ function reverseInteger(num) {
  *   4916123456789012 => false
  */
 function isCreditCardNumber(ccn) {
-	throw new Error('Not implemented');
+	const to_digits = numString =>
+		numString
+			.replace(/[^0-9]/g, '')
+			.split('')
+			.map(Number);
+
+	const condTransform = (predicate, value, fn) => {
+		if (predicate) {
+			return fn(value);
+		} else {
+			return value;
+		}
+	};
+
+	const doubleEveryOther = (current, idx) =>
+		condTransform(idx % 2 === 0, current, x => x * 2);
+
+	const reduceMultiDigitVals = current =>
+		condTransform(current > 9, current, x => x - 9);
+
+	const validate = numString => {
+		const digits = to_digits(numString);
+		const len = digits.length;
+		const luhn_digit = digits[len - 1];
+
+		const total = digits
+			.slice(0, -1)
+			.reverse()
+			.map(doubleEveryOther)
+			.map(reduceMultiDigitVals)
+			.reduce((current, accumulator) => current + accumulator, luhn_digit);
+
+		return total % 10 === 0;
+	};
+
+	return validate(ccn.toString());
 }
 
 
