54ccff35124136626e81ab84f0b8342d58ba019b
verenichyi <verenichyi@gmail.com>
2023-02-07 12:55:46 +0300

    feat: implement toStringList func

--- js/practice/task/04-arrays-tasks.js
+++ js/practice/task/04-arrays-tasks.js
@@ -433,7 +433,7 @@ function findAllOccurences(arr, item) {
  *    ['rock', 'paper', 'scissors']     => 'rock,paper,scissors'
  */
 function toStringList(arr) {
-	throw new Error('Not implemented');
+	return arr.join(',')
 }
 
 
