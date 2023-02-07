f1db898fdbca47e18424143ff745000ac35a0884
verenichyi <verenichyi@gmail.com>
2023-02-07 11:30:27 +0300

    feat: implement getMovingSum func

--- js/practice/task/04-arrays-tasks.js
+++ js/practice/task/04-arrays-tasks.js
@@ -243,7 +243,8 @@ function toArrayOfSquares(arr) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
 function getMovingSum(arr) {
-	throw new Error('Not implemented');
+	let sum = 0;
+	return arr.map((item) => sum += item);
 }
 
 /**
