41ccc417774a973505732064ae24ba194a948d3f
verenichyi <verenichyi@gmail.com>
2023-02-08 21:56:28 +0300

    feat: implement `getIdGeneratorFunction` func

--- js/practice/task/09-functions-n-closures-tasks.js
+++ js/practice/task/09-functions-n-closures-tasks.js
@@ -171,7 +171,7 @@ function partialUsingArguments(fn) {
  *   getId10() => 11
  */
 function getIdGeneratorFunction(startFrom) {
-	throw new Error('Not implemented');
+	return () => startFrom++;
 }
 
 
