9f2f074ff19f28084e6987c001e3e367506d9372
verenichyi <verenichyi@gmail.com>
2023-02-09 11:37:59 +0300

    feat: implement `memoize` func

--- js/practice/task/09-functions-n-closures-tasks.js
+++ js/practice/task/09-functions-n-closures-tasks.js
@@ -84,7 +84,14 @@ function getPolynom() {
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
 function memoize(func) {
-	throw new Error('Not implemented');
+	let cache;
+	return () => {
+		if (func) {
+			cache = func();
+			func = null;
+		}
+		return cache;
+	}
 }
 
 
